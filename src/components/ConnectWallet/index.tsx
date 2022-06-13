5f04c0ee73ecb132f926a9618bb95251a5d0a7b2
zhaobowen <1964471757@qq.com>
2022-06-13 17:49:18 +0800

    完成链接钱包的布局

--- src/components/ConnectWallet/index.tsx
+++ src/components/ConnectWallet/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import style from './index.module.css';
 export default function index() {
   return (
-    <div>
-      <span>图片</span>
-      等待链接钱包
+    <div className={style.connectWallte}>
+      <div className={style.loading }>
+        <span>图片-</span>
+        等待链接钱包
+      </div>
     </div>
   )
 }
