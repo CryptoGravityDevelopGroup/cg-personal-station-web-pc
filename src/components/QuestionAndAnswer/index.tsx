99f8d43c3756694e137050c183f63109ed7a92ae
zhaobowen <1964471757@qq.com>
2022-06-16 00:08:03 +0800

    优化样式

--- src/components/QuestionAndAnswer/index.tsx
+++ src/components/QuestionAndAnswer/index.tsx
@@ -26,34 +26,40 @@ export default function Index() {
   }
   return (
     <>
-      {
-        questionList.map((item, index) => {
-          return (
-            <div key={index} className={style.qaItem}>
-              <div className={style.deletBtn} onClick={() => {
-                deleteQuestion();
-              }}>
-                <Image src={deleteBtnPic} alt='deleteBtnPic'/>
+      <div className={style.wrap}>
+        {
+          questionList.map((item, index) => {
+            return (
+              <div key={index} className={style.qaItem}>
+                <div className={style.deletBtn} onClick={() => {
+                  deleteQuestion();
+                }}>
+                  <Image src={deleteBtnPic} alt='deleteBtnPic'/>
+                </div>
+                <div className={style.title}>
+                  question {index + 1}
+                </div>
+                <Input style={{marginBottom: '24px',}} />
+                <div className={style.title}>
+                  answer
+                </div>
+                <TextArea placeholder="Enter your Answer" autoSize={{ minRows: 4, maxRows: 4 }} />
               </div>
-              <div className={style.title}>
-                question 1
-              </div>
-              <Input style={{marginBottom: '24px',}} />
-              <div className={style.title}>
-                answer
-              </div>
-              <TextArea placeholder="Enter your Answer" autoSize={{ minRows: 4, maxRows: 4 }} />
-            </div>
-          )
-        })
-      }
-      <div className={style.addQuestion} onClick={() => {
-          addQuestion()
-        }}>
-        <div className={style.addBtnPic}>
-          <Image src={addBtnPic} alt='addBtnPic'/>
+            )
+          })
+        }
+        <div className={style.addQuestion} onClick={() => {
+            addQuestion()
+          }}>
+          <div className={style.addBtnPic}>
+            <Image src={addBtnPic} alt='addBtnPic'/>
+          </div>
+          <span className={style.addContent}>add</span>
         </div>
-        <span className={style.addContent}>add</span>
+        
+        <div className={'button'} onClick={() => {
+          console.log('Done')
+        }}>Done</div>
       </div>
     </>
   )
